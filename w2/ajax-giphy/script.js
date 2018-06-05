aff123e2701c50d5cd7900961dd51bc0e4cd5e72
Zack Fornaca <zfornaca@gmail.com>
2018-06-04 21:22:07 -0700

    giphy ajax

---
+++ w2/ajax-giphy/script.js
@@ -0,0 +1,22 @@
+$('form').on('submit', addGif);
+
+function addGif(evt) {
+  evt.preventDefault();
+  let searchTerm = $('#searchTerm').val();
+  console.log(searchTerm);
+  $.getJSON(
+    `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC`,
+    function(res) {
+      let gifSrc = res.data[0].images.fixed_height.url;
+      console.log(gifSrc);
+      let $newGif = $(`<img src="${gifSrc}">`);
+      $('#gifHolder').append($newGif);
+    }
+  );
+}
+
+$('#removeBtn').on('click', removeGifs);
+
+function removeGifs(evt) {
+  $('img').remove();
+}
